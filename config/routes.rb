be91af77a082a7f3f91c8099a8a77a36e05f0de6
Nelly Simkova <sk8high3r@ya.ru>
2023-12-18 10:20:07 +0300

    Add styling for Doctor model views

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   resources :schedules
   resources :departments
   resources :doctors
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "doctors#index"
 end
