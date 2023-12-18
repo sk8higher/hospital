a79a007b3933d2264331b7dd65ef8bd4691d7336
Nelly Simkova <sk8high3r@ya.ru>
2023-12-18 12:05:54 +0300

    Add styles for schedules

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,5 +1,7 @@
 class SchedulesController < ApplicationController
   before_action :set_schedule, only: %i[ show edit update destroy ]
+  before_action :set_doctors, only: %i[ new edit create update ]
+  before_action :set_departments, only: %i[ new edit create update ]
 
   # GET /schedules or /schedules.json
   def index
@@ -61,6 +63,14 @@ class SchedulesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_schedule
       @schedule = Schedule.find(params[:id])
+    end
+
+    def set_doctors
+      @doctors = Doctor.all
+    end
+
+    def set_departments
+      @departments = Department.all
     end
 
     # Only allow a list of trusted parameters through.
