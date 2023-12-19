9ad5cee68b66d97780d8dbdcae3d12b62b6a041b
Nelly Simkova <sk8high3r@ya.ru>
2023-12-19 11:08:18 +0300

    Add routes for downloads

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,13 @@ Rails.application.routes.draw do
   resources :departments
   resources :doctors
 
+  namespace :download do
+    resources :appointments, only: %i[index]
+    resources :patients, only: %i[index]
+    resources :schedules, only: %i[index]
+    resources :departments, only: %i[index]
+    resources :doctors, only: %i[index]
+    get "/all", to: "everything#index"
+  end
   root "doctors#index"
 end
